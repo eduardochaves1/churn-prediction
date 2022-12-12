e77c662c06265c48b1a4095bf83962d8298e2167
Eduardo Chaves <henriqueduardo2002@gmail.com>
2022-12-12 18:16:14 -0300

    project done

--- streamlit.py
+++ streamlit.py
@@ -7,9 +7,15 @@ import numpy as np
 st.write("""
   # Churn Prediction
 
-  - **Made by Eduardo Chaves - [GitHub](https://github.com/eduardochaves1)**
+  - **Made by [Eduardo Chaves](https://linkedin.com/in/edu-chaves 'LinkedIn de Eduardo Chaves')**
+  - **Repositório deste Projeto no [GitHub](https://github.com/eduardochaves1 'Churn Prediction by Eduardo Chaves')**
 
-  Esta página calcula a porcentagem de um cliente X *(cadastrado pelo usuário na barra lateral)* de evadir de uma empresa de telecomunicações *(churn)*.
+  ## Qual o problema a ser resolvido?
+  O intuito deste projeto é calcular a porcentagem de um cliente X *(cadastrado pelo usuário na barra lateral)* de cancelar ou continuar em um contrato (*plano*) de serviçoes de uma empresa de telecomunicações no próximo mês.
+
+  Tal situação é medido pelo *Churn*, que é quando um cliente deixa de consumir o produto / serviço de uma empresa. E este projeto visa capacitar uma empresa a identificar possíveis clientes que estão mais propensos a tomar tal decisão.
+  
+  Assim como identificar as métricas – relacionadas aos serviços / produtos da empresa – que mais influenciam na escolha do cliente sair ou continuar na mesma. Visando tomar medidas a diminuir o índice de *Churn* na empresa nos meses seguintes.
 """)
 
 st.image(Image.open('./assets/churnrate.jpg'))
@@ -18,10 +24,10 @@ st.image(Image.open('./assets/churnrate.jpg'))
 # SIDEBAR (user input_features)
 # ====================
 
-st.sidebar.header('**Cadastro dos Parâmetros**')
+st.sidebar.write('# **Cadastro dos Parâmetros**')
 
 def input_features():
-  gender = st.sidebar.radio('**- Sexo:**', ('Masculino', 'Feminino'), horizontal=True)
+  gender = st.sidebar.radio('**Sexo:**', ('Masculino', 'Feminino'), horizontal=True)
   seniorCitizen = st.sidebar.radio('**Idoso:**', ('Não', 'Sim'), horizontal=True)
   partner = st.sidebar.radio('**Casado/Namorando:**', ('Não', 'Sim'), horizontal=True)
   dependents = st.sidebar.radio('**Dependentes:**', ('Não', 'Sim'), horizontal=True)
@@ -37,8 +43,8 @@ def input_features():
   contract = st.sidebar.radio('**Tipo de Contrato:**', ('Mensal', 'Anual', 'Bianual'))
   paperlessBilling = st.sidebar.radio('**Cobrança Digital:**', ('Não', 'Sim'), horizontal=True)
   paymentMethod = st.sidebar.radio('**Método de Pagamento:**', ('Cartão de Crédito', 'Transferência Bancária', 'Cheque Eletrônico', 'Cheque Físico'))
-  tenure = st.sidebar.slider('**Tempo de Contratação (Meses):**', 0, 120, 12)
-  monthlyCharges = st.sidebar.number_input('**Cobrança Mensal (R$):**', value=65)
+  tenure = st.sidebar.slider('**Tempo de Contratação (Meses):**', 0, 70, 12)
+  monthlyCharges = st.sidebar.number_input('**Cobrança Mensal (R$):**', value=65, min_value=10, max_value=120)
   totalCharges = monthlyCharges * tenure
 
   data = {
@@ -69,6 +75,21 @@ def input_features():
 df = input_features()
 
 # ====================
+# MAIN CONTENT (first part)
+# ====================
+
+st.write("""
+  ## Como usar esta ferramenta?
+  Basta você cadastrar as variáveis na barra ao lado esquerdo e automaticamente ao manipular esses dados o modelo preditivo entrará em ação para trazer os resultados logo abaixo!
+
+  ## Parâmetros Cadastrados
+  - Abaixo segue os valores cadastrados pelo usuário na barra lateral:
+""")
+st.write(df)
+
+st.write('## Resultado da Predição')
+
+# ====================
 # DF PREPROCESSING (with user input_features)
 # ====================
 
@@ -88,23 +109,24 @@ df.PaymentMethod.replace(('Transferência Bancária', 'Cartão de Crédito', 'Ch
 # MODEL PREDICTION (with user input_features)
 # ====================
 
-model = joblib.load('churn-prediction-model')
+model = joblib.load('./assets/churn-prediction-model')
 predictionPercentage = np.round((model.predict_proba(df) * 100), 2)
 
 # ====================
-# MAIN CONTENT
+# MAIN CONTENT (last part)
 # ====================
-
-st.subheader('Cadastro dos Parâmetros')
-st.write('Os parâmetros estão todos em tipos númericos pois foram convertidos do texto cadastrado pelo usuário com o intuito de ter uma melhor predição pelo modelo previamente treinado nesta forma.')
-st.write(df)
-
-st.subheader('Resultado da Predição')
 
 predictions = [predictionPercentage[0][0], predictionPercentage[0][1]]
 
-predictionMsg = 'continuará na' if predictions[0] > 50 else 'sairá da'
+predictionMsg = '***continuará*** no' if predictions[0] > 50 else '***cancelará*** o'
 predictionNumber = predictions[0] if predictions[0] > 50 else predictions[1]
 
-st.write('O modelo previu uma porcentagem de', predictionNumber, '% de que o cliente', predictionMsg, 'empresa!')
+st.write('O modelo previu uma porcentagem de', predictionNumber, '% de que o cliente', predictionMsg, 'contrato!')
 st.write(predictionPercentage)
+st.write('- 0 = Not Churn | 1 = Churn')
+
+st.write("""
+  ## Como o Modelo Chegou a este Resultado?
+  Segue abaixo um gráfico exibindo as variáveis que mais importam na tomada de decisão de acordo com o aprendizado do modelo treinado previamente.
+""")
+st.image(Image.open('./assets/feature-importance.png'))
